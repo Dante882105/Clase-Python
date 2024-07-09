9ae428d60cd443a5c572efade641c891d27ecd9d
Dante882105 <danberjim13@hotmail.com>
2024-07-09 12:00:07 -0500

    feat: Se agregan ejercicios del día uno

---
+++ Primera Clase/primerArchivo.py
@@ -0,0 +1,14 @@
+#Función Print
+print("Hola Mundo te digo 'Hola'")
+print( 100 +25.2)
+
+#Strings
+print("esta es \\ una barra invertida")
+print("Me llamo \"Daniel\" jejeje")
+print("Ésta es una línea \n y Ésta es otra línea")
+print("\tTabulación")
+print('this isn\'t a number')
+
+#Input
+print("Tu nombre es: " + (input("Dime tu nombre: ")) + " Y tu apellido es: " + input("Dime tu apellido: "))
+
