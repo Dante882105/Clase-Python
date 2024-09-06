1673551e06bdeb7b44d1f54e83a9f5f6ff5d5092
Dante882105 <danberjim13@hotmail.com>
2024-09-05 23:06:14 -0500

    feat: se agregan hasta el tema 8 Sets

--- 3. Tercera clase/método_index/index.py
+++ 3. Tercera clase/método_index/index.py
@@ -76,4 +76,70 @@ poema = """Mil pequeños peces blancos
 como si hirvieran
 el color del agua
 """
-print(len(poema))
+#print(len(poema))
+
+#######################################################################################################################################
+#clase 5 Listas o Arrays
+
+mi_lista = ["a", "b", "c"]
+mi_lista2 = ["d", "e", "f" ]
+#Eliminar un elemento
+eliminado = mi_lista.pop(1)
+#Agregar un elemento
+mi_lista2.append("g")
+resultado = len(mi_lista)
+#print(mi_lista + mi_lista2)
+print(mi_lista, eliminado)
+
+#############################################################################################################################################
+
+#Clase 6 Diccionarios u Objetos
+
+diccionario = {"c1":"Valor 1", "c2": "Valor 2"}
+resultado = diccionario["c1"]
+
+cliente = {"nombre": "Ángel", "Apellido": "Bernal", "peso": "7900 gr", "talla": 3}
+consulta = cliente["Apellido"]
+
+dic = {"c1": 55, "c2": [10, 20, 30], "c3": {"s1": 100, "s2": 200}}
+
+dic2 = {"c1":["a","b","c"], "c2":["d", "e", "f"]}
+
+dic3 = {1: "a", 2 : "b"}
+# agregar un valor
+dic3[3] = 'c'
+# reemplazar un valor
+dic3[2] = "B"
+
+#print(dic["c2"])
+#print(dic["c2"][1])
+#print(dic["c3"]["s2"])
+#print(dic2["c2"][1].upper())
+#print(dic3)
+
+#############################################################################################################################################
+#Clase 7 Tuples
+
+#mi_tuple = ("Hola", "Mundo", "Developer", "Python")
+#mi_tuple2 = "Hola", "Mundo", "Developer", "Python"
+
+mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+#Método usado para contar cantidad de veces que aparece un valor
+print(mi_tupla.count(2))
+#print(type(mi_tuple2))
+
+#############################################################################################################################################
+#Clase 8 Sets, se pueden declarar como "set([])" o sin palabra clave y con corchetes "{}", tampoco recibe elementos duplicados y si se hace, Python los descartará
+
+mi_set = set([1,2,3,4,5,6])
+#print(type(mi_set), mi_set) 
+
+mi_set2 = {1,2,3,4,5,7,8}
+#print(type(mi_set2), mi_set2)
+
+mi_set.add(10)
+
+mi_set2.discard(1)
+
+union = mi_set.union(mi_set2)
+print(union)
